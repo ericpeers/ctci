662944723f0db115599b9aa505e82178e5eda7a3
Eric Peers <peers@mtnboy.net>
2017-11-09 21:19:00 -0700

    tweaking more from PR's

--- string_unique/README.md
+++ string_unique/README.md
@@ -52,3 +52,4 @@ Approximately 2 hours, with two minor interruptions. Lots of mvn learning, had t
 * mvn setup, including the default example on apache is not valid. New mvn requires package.
 * Basic testing in java
 * That an editor that fixes imports for me is useful. Emacs not really cutting it. But making me think a bit.
+* Extended chars are not trivial. Would prefer to be able to escape them via a hex code in a string.
--- string_unique/string-app/src/test/java/net/mtnboy/StringUnique/AppTest.java
+++ string_unique/string-app/src/test/java/net/mtnboy/StringUnique/AppTest.java
@@ -32,16 +32,30 @@ public class AppTest
     {
         assertTrue("check null", App.hasAllUnique(null));
         assertTrue("check empty", App.hasAllUnique(""));
+        assertTrue("check null", App.hasAllUnique2(null));
+        assertTrue("check empty", App.hasAllUnique2(""));
+
+        //what I want is something more like:
+        /*
+            foreach method in (hasAllUnique, hasAllUnique2) {
+                assertTrue("check null", App.method(null));
+                ...
+            }
+        */
     }
 
     public void testNums() {
         assertTrue("check nums",  App.hasAllUnique("01234{}"));
         assertFalse("check nums with dup",  App.hasAllUnique("01234{}0"));
+        assertTrue("check nums",  App.hasAllUnique2("01234{}"));
+        assertFalse("check nums with dup",  App.hasAllUnique2("01234{}0"));
     }
 
     public void testCase() {
         assertTrue("check mixed case", App.hasAllUnique("abcABC"));
         assertFalse("check upcase dups", App.hasAllUnique("ABCABC"));
+        assertTrue("check mixed case", App.hasAllUnique2("abcABC"));
+        assertFalse("check upcase dups", App.hasAllUnique2("ABCABC"));
     }
 
     //skip long string test because I'm lazy to build a unit test for this. TBD with another test framework.
@@ -49,6 +63,8 @@ public class AppTest
     public void testShort() {
         assertTrue("check singleton", App.hasAllUnique("a"));
         assertFalse("check dup, but short", App.hasAllUnique("aa"));
+        assertTrue("check singleton", App.hasAllUnique2("a"));
+        assertFalse("check dup, but short", App.hasAllUnique2("aa"));
     }
 
     public void testDup() {
@@ -56,12 +72,19 @@ public class AppTest
         assertFalse(App.hasAllUnique("heloh"));
         assertFalse(App.hasAllUnique("eehlo"));
         assertFalse(App.hasAllUnique("ehloh"));
+        assertFalse(App.hasAllUnique2("hello"));
+        assertFalse(App.hasAllUnique2("heloh"));
+        assertFalse(App.hasAllUnique2("eehlo"));
+        assertFalse(App.hasAllUnique2("ehloh"));
     }
 
     public void testNoDup() {
         assertTrue("simple no dup", App.hasAllUnique("abcdefghijklmno"));
         assertTrue("upcase no dup", App.hasAllUnique("ABCDEFG"));
         assertTrue("mixcase, no dup", App.hasAllUnique("abcCBA"));
+        assertTrue("simple no dup", App.hasAllUnique2("abcdefghijklmno"));
+        assertTrue("upcase no dup", App.hasAllUnique2("ABCDEFG"));
+        assertTrue("mixcase, no dup", App.hasAllUnique2("abcCBA"));
     }
 
     public void testWhiteSpace() {
@@ -70,4 +93,16 @@ public class AppTest
         assertTrue("mixed space, no dup", App.hasAllUnique(" \t"));
         assertTrue("mixed space no dup2", App.hasAllUnique(" \n"));
     }
+
+    public void testExtendedChars() {
+        char [] extended = new char[3];
+        extended[0] = 236;
+        extended[1] = 219;
+        extended[2] = 202;
+        String extendedStr = new String(extended);
+        assertTrue("extended chars", App.hasAllUnique(extendedStr));
+
+        extended[2] = 236;
+        extendedStr = new String(extended);
+        assertFalse("extended chars", App.hasAllUnique(extendedStr));}
 }
